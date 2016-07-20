6f5d465b4ecce4079b6d64bfe3608d213c697010
olga.ivanko <o.ivanko@me.com>
2016-07-20 10:14:28 +0300

    Add project files.

---
+++ src/javajdbc1/DatabaseTableModel.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javajdbc1;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author homefulloflove
+ */
+public class DatabaseTableModel extends AbstractTableModel {
+
+    private List<String> columnNames = new ArrayList();
+    private ArrayList columnTypes = new ArrayList();
+    private ArrayList data = new ArrayList();
+    private boolean editable;
+
+    public DatabaseTableModel(boolean editable) {
+        this.editable = editable;
+    }
+
+    @Override
+    public int getRowCount() {
+        synchronized (data) {
+            return data.size();
+        }
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnNames.size();
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columnNames.get(column);
+    }
+
+    @Override
+    public Object getValueAt(int row, int column) {
+        synchronized (data) {
+            return ((ArrayList) data.get(row)).get(column);
+        }
+    }
+
+    @Override
+    public void setValueAt(Object value, int row, int column) {
+        synchronized (data) {
+            ((ArrayList) data.get(row)).set(column, value);
+        }
+    }
+
+//    public ArrayList setColumnName(ResultSet rsColName) {
+//        try {
+//            while (rsColName.next()) {
+//                columnNames.add(rsColName.getString(2));
+//                System.out.println(rsColName.getString(2));
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(DatabaseTableModel.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return columnNames;
+//    }
+    public void setDataSource(ResultSet rs) throws Exception {
+        data.clear();
+        columnNames.clear();
+        columnTypes.clear();
+
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnCount = rsmd.getColumnCount();
+        for (int i = 0; i < columnCount; i++) {
+
+            columnNames.add(rsmd.getColumnName(i + 1));
+            System.out.println(rsmd.getColumnName(i + 1));
+            Class type = Class.forName(rsmd.getColumnClassName(i + 1));
+            columnTypes.add(type);
+
+        }
+//        columnNames = setColumnName(rsColName); // в параметрах setDataSource был еще один ResultSet
+
+        fireTableStructureChanged();
+
+        while (rs.next()) {
+
+            ArrayList row = new ArrayList();
+            for (int i = 0; i < columnCount; i++) {
+                if (columnTypes.get(i) == String.class) {
+                    row.add(rs.getString(i + 1));
+                } else {
+                    row.add(rs.getObject(i + 1));
+                }
+            }
+            synchronized (data) {
+                data.add(row);
+
+                fireTableRowsInserted(data.size() - 1, data.size() - 1);
+            }
+        }
+    }
+
+}
---
+++ src/javajdbc1/Main.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javajdbc1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.concurrent.locks.StampedLock;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author homefulloflove
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        String conn = "jdbc:sqlite:database.sl3";
+        
+        try(Connection ctn = DriverManager.getConnection(conn)){
+           
+            Statement stmt = ctn.createStatement();
+            
+            stmt.execute("CREATE TABLE IF NOT EXISTS contacts (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone_number TEXT)");
+            
+//            stmt.executeUpdate("INSERT INTO contacts VALUES (null, 'Olga', '0932066751')");
+//            stmt.executeUpdate("INSERT INTO contacts VALUES (null, 'Vasia', '0675432878')");
+//            stmt.executeUpdate("INSERT INTO contacts VALUES (null, 'Alex', '0673270889')");
+            
+            stmt.executeUpdate("DELETE FROM contacts WHERE id = 4");
+            
+            
+            if (stmt.execute("SELECT * FROM contacts")) {
+                ResultSet rs = stmt.getResultSet();
+                while(rs.next()){
+                    // +380 (92) 123-45-67
+                    System.out.printf("#%d name: %s, phone number: %s\n",rs.getInt(1), rs.getString(2), rs.getString(3));
+                }
+            }
+            
+            ResultSet eq = stmt.executeQuery("SELECT * FROM contacts");
+            
+//            System.out.println(eq);
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
